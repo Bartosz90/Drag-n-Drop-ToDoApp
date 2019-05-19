393ab2719fd8a46cef26469369f08b0a61548283
Bartosz90 <36510019+Bartosz90@users.noreply.github.com>
2019-05-19 20:47:10 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,93 @@
+const addBtn = document.querySelector('form button');
+const inputTask = document.querySelector('input');
+const ul = document.querySelector('.task-holder ul');
+let taskLiPosition = 13;
+const colors = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsis', 'lime', 'teal', 'aqua', 'blue', 'navy', 'silver', 'white'];
+const toDoList = [];
+//Add a task!
+
+const addTask = (e) => {
+    e.preventDefault();
+    if (!inputTask.value) {
+        alert("Add task content!");
+        return
+    };
+    const newLi = document.createElement('li');
+    const removeDiv = document.createElement('div');
+    newLi.className = 'task';
+    toDoList.push(newLi);
+    renderList();
+    console.log(toDoList);
+    ul.appendChild(newLi);
+    newLi.textContent = inputTask.value;
+    newLi.appendChild(removeDiv);
+    removeDiv.className = 'remove';
+    newLi.style.top = `${taskLiPosition}vh`;
+    newLi.style.border = `2px solid ${colors[Math.floor(Math.random()*colors.length)]}`
+    taskLiPosition++;
+    inputTask.value = '';
+
+    //move the task!
+
+    let drawActive = false;
+    let insertLiX;
+    let insertLiY;
+    newLi.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        newLi.style.backgroundColor = "black";
+        drawActive = !drawActive;
+        insertLiX = e.offsetX;
+        insertLiY = e.offsetY;
+    });
+
+    newLi.addEventListener('mousemove', (e) => {
+        if (drawActive) {
+            e.preventDefault();
+            divX = e.clientX - insertLiX;
+            divY = e.clientY - insertLiY;
+            newLi.style.left = `${divX}px`;
+            newLi.style.top = `${divY}px`;
+        }
+    })
+    newLi.addEventListener('mouseup', (e) => {
+        e.preventDefault();
+        newLi.style.backgroundColor = "#303f42";
+        drawActive = !drawActive;
+
+    })
+
+    //remove the task!
+
+    const removeTask = (e) => {
+        e.target.parentNode.remove();
+        toDoList.splice(index, 1)
+        console.log(toDoList);
+        renderList();
+    }
+    removeDiv.addEventListener('click', removeTask);
+};
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        ul.appendChild(toDoElement);
+    })
+}
+
+addBtn.addEventListener('click', addTask);
+
+//search the task, highlight the right one
+const searchInput = document.querySelector('.search');
+const liElements = document.querySelectorAll('li');
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    toDoList.forEach((li, index) => {
+        if (!searchText) {
+            toDoList[index].style.boxShadow = "";
+        } else if (li.textContent.toLowerCase().includes(searchText)) {
+            toDoList[index].style.boxShadow = `3px 2px 19px 10px ${toDoList[index].style.borderColor}`;
+        } else if (!li.textContent.toLowerCase().includes(searchText)) {
+            toDoList[index].style.boxShadow = "";
+        }
+    })
+}
+searchInput.addEventListener('input', searchTask);
